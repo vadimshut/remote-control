5816378c522482cea93079f2e8793b4e96807fb8
Vadzim Shut <vadzim_shut@epam.com>
2022-06-25 16:07:02 +0300

    feat: implement connect and disconnect to wss

--- src/wss/onConnect.ts
+++ src/wss/onConnect.ts
@@ -1,4 +1,14 @@
+import { WebSocket, WebSocketServer } from 'ws';
+
 export const onConnect = (ws: WebSocket) => {
     console.log("New user connected to WS");
     ws.send("Connected to WS...");
+
+    ws.on("message", function (message: Buffer) {
+        console.log(message);
+        
+    })
+    ws.on('close', () => {
+        console.log("WS connection lost...\n");
+    })
 }
